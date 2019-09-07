927f4f20323ed49cd5fe375df251a43d195ecdfe
Thomas Caraway <tcareaway@gmail.com>
2019-09-07 13:56:34 -0500

    Added SDWebImage via CocoaPods

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9.0'
+
+target 'PixaPicker' do
+  use_frameworks!
+
+pod 'SDWebImage', '~> 5.0'
+
+end
---
+++ Pods/Target Support Files/Pods-PixaPicker/Pods-PixaPicker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PixaPicker : NSObject
+@end
+@implementation PodsDummy_Pods_PixaPicker
+@end
