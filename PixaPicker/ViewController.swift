2bd2e58dfe058fde3a6eec267f8735faa2a4ef59
Thomas Caraway <tcareaway@gmail.com>
2019-09-19 14:04:15 -0500

    String manipulation for search text

--- PixaPicker/ViewController.swift
+++ PixaPicker/ViewController.swift
@@ -11,10 +11,16 @@ import SDWebImage
 
 class ViewController: UIViewController, UISearchResultsUpdating, UISearchBarDelegate {
     
+    let apiKey = "key=13683470-874d69ddffa828cbb82551e32" //API key for my Pixabay account
     @IBOutlet weak var picsCollection: UICollectionView!
     
     func updateSearchResults(for searchController: UISearchController) {
-        
+        let searchText = searchController.searchBar.text!
+        let searchTextWithoutSpaces = searchText.replacingOccurrences(of: " ", with: "+")
+        let urlWithAPI = "https://pixabay.com/api/?" + apiKey
+        let urlWithSeachText = urlWithAPI + "&q=" + searchTextWithoutSpaces
+        let finalURL = urlWithSeachText + "&image_type=photo"
+        print(finalURL) //FOR TESTING ONLY
     }
     
     override func viewDidLoad() {
@@ -26,6 +32,7 @@ class ViewController: UIViewController, UISearchResultsUpdating, UISearchBarDele
         searchController.searchBar.placeholder = "Image Search"
         navigationItem.searchController = searchController
         searchController.searchBar.sizeToFit()
+        navigationItem.hidesSearchBarWhenScrolling = false
         
         
         // Do any additional setup after loading the view.
