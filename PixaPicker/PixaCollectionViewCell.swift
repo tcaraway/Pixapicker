0638ad1de142a1ad8bab04c93ab4346a55741be0
Thomas Caraway <tcareaway@gmail.com>
2019-10-20 13:50:07 -0500

    images loading and appearing

---
+++ PixaPicker/PixaCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PixaCollectionViewCell.swift
+//  PixaPicker
+//
+//  Created by Thomas Caraway on 10/18/19.
+//  Copyright © 2019 Thomas Caraway. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PixaCollectionViewCell : UICollectionViewCell {
+    @IBOutlet weak var cellImage: UIImageView!
+}
+
+
